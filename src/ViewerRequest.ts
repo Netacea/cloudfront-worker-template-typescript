0b3e191da2549c335dd08a506245bb22f6345cba
Scotsoo <jack.scotson@netacea.com>
2021-01-12 14:59:39 +0000

    Adding github actions and comments in code.

--- src/ViewerRequest.ts
+++ src/ViewerRequest.ts
@@ -3,10 +3,12 @@ import * as NetaceaConfig from './NetaceaConfig.json'
 const worker = new Cloudfront(NetaceaConfig as CloudfrontConstructorArgs)
 
 export const handler = async (event: any, context: any, callback: any): Promise<any> => {
+  // Netacea's worker needs to be run first
   const netaceaResponse = await worker.run(event)
   const { request, response } = netaceaResponse.Records[0].cf
   if (response !== undefined) {
     return callback(null, response)
   }
+  // Your code here
   return callback(null, request)
 }
