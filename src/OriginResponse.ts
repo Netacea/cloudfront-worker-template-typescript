7ee80a615b1779390cef1fbda492073e30886e51
Luke Robinson <luke.robinson@netacea.com>
2021-02-03 12:07:59 +0000

    changed viewer response to check for correct status code and added origin repsonse for status of 400 and above in origin

--- src/OriginResponse.ts
+++ src/OriginResponse.ts
@@ -5,11 +5,12 @@ const worker = new Cloudfront(NetaceaConfig as CloudfrontConstructorArgs)
 export const handler = async (event: any, context: any, callback: any): Promise<void> => {
   // Your code here
 
-  // These should be ran at the very end of the ViewerResponse, just before calling the callback.
-  if (event?.Records[0]?.cf?.response?.status < 400) {
+  // These should be ran at the very end of the OriginResponse, just before calling the callback.
+  if (event?.Records[0]?.cf?.response?.status >= 400) {
     worker.addNetaceaCookiesToResponse(event)
     worker.ingest(event)
     return callback(null, event.Records[0].cf.response)
   }
   return callback()
+  
 }
