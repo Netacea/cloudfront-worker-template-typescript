e40b0404229e84bce5a1ba48c33cecb0f976b97e
Luke Robinson <luke.robinson@netacea.com>
2021-02-03 12:13:32 +0000

    fix linting

--- src/ViewerResponse.ts
+++ src/ViewerResponse.ts
@@ -11,5 +11,6 @@ export const handler = async (event: any, context: any, callback: any): Promise<
     worker.ingest(event)
     return callback(null, event.Records[0].cf.response)
   }
+
   return callback()
 }
