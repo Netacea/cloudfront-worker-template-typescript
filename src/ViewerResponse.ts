6b8edad14ce43e827517b7af7c175c558c642d01
Scotsoo <jack.scotson@netacea.com>
2021-08-04 15:08:34 +0100

    Allowing 2 floating promises

--- src/ViewerResponse.ts
+++ src/ViewerResponse.ts
@@ -9,6 +9,7 @@ export const handler = async (event: any, context: any, callback: any): Promise<
   // These should be ran at the very end of the ViewerResponse, just before calling the callback.
   if (event.Records[0].cf.response.status < 400) {
     worker.addNetaceaCookiesToResponse(event)
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
     worker.ingest(event)
   }
 
